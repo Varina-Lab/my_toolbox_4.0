5769babb7434f1052e55862ba117e6da12d04e7c
Varina-Lab <varina.lab@proton.me>
2026-04-26 21:01:45 +0700

    Cập nhật

--- zig/portable_run/build.zig
+++ zig/portable_run/build.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
+    const optimize = b.standardOptimizeOption(.{}); 
 
     const exe = b.addExecutable(.{
         .name = "portable_run",
@@ -10,9 +10,6 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
-
-    exe.root_module.strip = optimize != .Debug;
-    exe.want_lto = true;
 
     b.installArtifact(exe);
 }
