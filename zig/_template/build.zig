8e87f0226055a309c934bbcf20c386fe9cd2098d
Varina-Lab <varina.lab@proton.me>
2026-04-26 20:09:44 +0700

    Update build.zig

--- zig/_template/build.zig
+++ zig/_template/build.zig
@@ -2,11 +2,10 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-    // Mặc định ép ReleaseSmall hoặc ReleaseFast từ CLI
     const optimize = b.standardOptimizeOption(.{}); 
 
     const exe = b.addExecutable(.{
-        .name = "zig-tool",
+        .name = "_template",
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
